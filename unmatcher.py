a3b31adf76785bec6f75692a7fffb6a909e18e99
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-08-22 21:41:45 +0200

    Barebone implementation supporting only literal strings

--- unmatcher.py
+++ unmatcher.py
@@ -6,6 +6,7 @@ Regular expression reverser for Python
 
 :author: Karol Kuczmarski "Xion"
 """
+import random
 import re
 
 
@@ -26,5 +27,26 @@ class Unmatcher(object):
     def __call__(self, *args, **kwargs):
         return self.reverse(*args, **kwargs)
 
-    def reverse(self):
-        raise NotImplementedError()
+    def reverse(self, groups=None, **kwargs):
+        reversal = RegexReversal(self.regex_ast,
+                                 groups=groups or {}, **kwargs)
+        return reversal.perform()
+
+
+# Implementation
+
+class RegexReversal(object):
+
+    def __init__(self, regex_ast, **kwargs):
+        self.regex_ast = regex_ast
+        self.capture_groups = kwargs.get('groups', {})  # NYI
+
+    def perform(self):
+        # TODO: preserve unicodeness/ANSIness in Python 2
+        return ''.join(map(self._reverse_node, self.regex_ast))
+
+    def _reverse_node(self, (type_, data)):
+        if type_ == 'literal':
+            return chr(data)
+
+        raise ValueError("unsupported regular expression element: %s" % type_)
