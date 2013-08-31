bc473e42a67f9a2a9203f6da696a3667013cdd3d
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2013-08-31 19:53:44 +0200

    Tests for everything but (non-)capture groups

--- test_unmatcher.py
+++ test_unmatcher.py
@@ -8,9 +8,12 @@ import unmatcher
 import pytest
 
 
-DEFAULT_TESTS_COUNT = 256
+DEFAULT_TESTS_COUNT = 32
+DEFAULT_CHARACTER_SETS = ('ascii_letters', 'digits')
 
-str_arg = lambda name: {name: str, 'str_attrs': ('ascii_letters', 'digits')}
+str_arg = lambda name: {name: str, 'str_attrs': DEFAULT_CHARACTER_SETS}
+str_args = lambda *names: dict([(name, str) for name in names] +
+                               [('str_attrs', DEFAULT_CHARACTER_SETS)])
 
 
 @pytest.mark.randomize(ncalls=DEFAULT_TESTS_COUNT, **str_arg('expr'))
@@ -26,8 +29,51 @@ def test_any(_, flags):
     assert bool(dot_re.match(reversed_dot))
 
 
-# TODO: test charset ranges: [abc], [a-z], \d
-# TODO: test branching: a|b, a|b|c, a|b|\d, etc.
+@pytest.mark.randomize(ncalls=DEFAULT_TESTS_COUNT, **str_args('left', 'right'))
+def test_branch_double(left, right):
+    branch_re = re.compile('|'.join(map(re.escape, (left, right))))
+    reversed_branch = unmatcher.reverse(branch_re)
+    assert reversed_branch in (left, right)
+
+
+@pytest.mark.randomize(ncalls=DEFAULT_TESTS_COUNT,
+                       **str_args('first', 'second', 'third'))
+def test_branch_triple(first, second, third):
+    branch_re = re.compile('|'.join(map(re.escape, (first, second, third))))
+    reversed_branch = unmatcher.reverse(branch_re)
+    assert reversed_branch in (first, second, third)
+
+
+# TODO: eliminate repetition in above tests by devising a more general variant
+#       that would also verify a more diverse set of test cases
+
+
+@pytest.mark.randomize(ncalls=DEFAULT_TESTS_COUNT,
+                       chars=str, str_attrs=('ascii_letters',))
+def test_charset_specified(chars):
+    if not chars:
+        return
+    charset_re = re.compile('[%s]' % chars)
+    reversed_charset = unmatcher.reverse(charset_re)
+    assert reversed_charset in chars
+
+
+@pytest.mark.randomize(ncalls=DEFAULT_TESTS_COUNT,
+                       minchar=str, maxchar=str,
+                       fixed_length=1, str_attrs=('ascii_letters',))
+def test_charset_range(minchar, maxchar):
+    minchar, maxchar = min(minchar, maxchar), max(minchar, maxchar)
+    charset_re = re.compile('[%s-%s]' % (minchar, maxchar))
+    reversed_charset = unmatcher.reverse(charset_re)
+    assert minchar <= reversed_charset <= maxchar
+
+
+@pytest.mark.randomize(ncalls=DEFAULT_TESTS_COUNT,
+                       class_=str, choices='wds')
+def test_charset_class(class_):
+    charset_re = re.compile(r'\%s' % class_)
+    reversed_charset = unmatcher.reverse(charset_re)
+    assert bool(charset_re.match(reversed_charset))
 
 
 @pytest.mark.parametrize('symbol', ('+', '*'))
@@ -69,8 +115,6 @@ def test_repeat_range(lower_bound, upper_bound, char):
     for chunk in chunks(reversed_repeat, len(char)):
         assert char == chunk
 
-
-# TODO: test repeat range without upper_bound: foo{2}, bar{7}, etc.
 
 # TODO: test non-capture groups: (?:foo)bar
 # TODO: test capture groups without backreferences: (foo)bar, (?P<huh>foo)bar
