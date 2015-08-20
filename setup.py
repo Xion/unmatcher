c9a03b92a25fa83f19a2d2fe792f9bf4935360bb
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2015-08-19 23:03:12 -0700

    Don't import the unmatcher module in setup.py

--- setup.py
+++ setup.py
@@ -5,19 +5,86 @@ unmatcher
 
 Regular expression reverser for Python
 """
+import ast
+import os
 from setuptools import setup
+import sys
 
-import unmatcher
 
+# Utility functions
+
+def read_tags(filename):
+    """Reads values of "magic tags" defined in the given Python file.
+
+    :param filename: Python filename to read the tags from
+    :return: Dictionary of tags
+    """
+    with open(filename) as f:
+        ast_tree = ast.parse(f.read(), filename)
+
+    res = {}
+    for node in ast.walk(ast_tree):
+        if type(node) is not ast.Assign:
+            continue
+
+        target = node.targets[0]
+        if type(target) is not ast.Name:
+            continue
+        if not (target.id.startswith('__') and target.id.endswith('__')):
+            continue
+
+        name = target.id[2:-2]
+        res[name] = ast.literal_eval(node.value)
+
+    return res
+
+
+def read_requirements(filename='requirements.txt'):
+    """Reads the list of requirements from given file.
+
+    :param filename: Filename to read the requirements from.
+                     Uses ``'requirements.txt'`` by default.
+
+    :return: Requirements as list of strings
+    """
+    # allow for some leeway with the argument
+    if not filename.startswith('requirements'):
+        filename = 'requirements-' + filename
+    if not os.path.splitext(filename)[1]:
+        filename += '.txt'  # no extension, add default
+
+    def valid_line(line):
+        line = line.strip()
+        return line and not any(line.startswith(p) for p in ('#', '-'))
+
+    def extract_requirement(line):
+        egg_eq = '#egg='
+        if egg_eq in line:
+            _, requirement = line.split(egg_eq, 1)
+            return requirement
+        return line
+
+    with open(filename) as f:
+        lines = f.readlines()
+        return list(map(extract_requirement, filter(valid_line, lines)))
+
+
+# setup() call
+
+tags = read_tags('unmatcher.py')
+
+tests_require = read_requirements('test')
+if sys.version_info < (2, 7):
+    tests_require.extend(read_requirements('test-py26'))
 
 setup(
     name="unmatcher",
-    version=unmatcher.__version__,
+    version=tags['version'],
     description="Regular expression reverser for Python",
     long_description=open('README.rst').read(),
-    author=unmatcher.__author__,
+    author=tags['author'],
     url="http://github.com/Xion/unmatcher",
-    license=unmatcher.__license__,
+    license=tags['license'],
 
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -32,5 +99,5 @@ setup(
     platforms='any',
     py_modules=['unmatcher'],
 
-    tests_require=open('requirements-test.txt').readlines(),
+    tests_require=tests_require,
 )
